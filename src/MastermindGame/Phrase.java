c2051f228bdbf2c11f428946597b1e9fe9a76985
ctg5117 <ctg5117@BKGAIGE208-53.win.psu.edu>
2018-04-04 15:41:00 -0400

    MainMenu Panel and Form created
    Phrase begun work

--- src/MastermindGame/Phrase.java
+++ src/MastermindGame/Phrase.java
@@ -15,11 +15,37 @@ public class Phrase
    private int intSize; 
    private char[] caPhrase = new char[intSize];
    
+   /**Defaults intSize to 5.
+    * 
+    */
    Phrase()
    {
-       
+       intSize = 5;
    }
    
    
+   
+   /*
+   Getter and Setter Block
+   */
+   public void setPhrase(char[] caInPhrase)
+   {
+       caPhrase = caInPhrase; 
+   }
+   
+   public char[] getPhrase()
+   {
+       return caPhrase;
+   }
+   
+   public void setSize(int intInSize)
+   {
+       intSize = intInSize;
+   }
+   
+   public int getSize()
+   {
+       return intSize;
+   }
    
 }
