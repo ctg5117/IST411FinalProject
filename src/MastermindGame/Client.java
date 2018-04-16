c5ed0111f9d340ed0d6ea1a08a54b06e05e33d62
Brady Shober <bvs5441@win.psu.edu>
2018-04-16 15:57:38 -0400

    Updated Client and Server connection

--- src/MastermindGame/Client.java
+++ src/MastermindGame/Client.java
@@ -11,10 +11,16 @@ import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -25,7 +31,14 @@ public class Client
     //Use run from this code http://cs.lmu.edu/~ray/notes/javanetexamples/
     
     //Open main menu
-    
+    public static void main(String[] args) {
+        JFMainMenu mainMenu = new JFMainMenu();
+        mainMenu.setVisible(true);
+        Client client = new Client();
+        JFGameDisplay game = new JFGameDisplay();
+        game.setVisible(true);
+        
+    }
     //Select Join game -> get ip of server -> server connected -> get 
     
     //
@@ -34,17 +47,50 @@ public class Client
     ObjectInputStream in;
     ObjectOutputStream out;
     
-    private String getServerAddress()
-    {
-        return JOptionPane.showInputDialog(null, "Enter IP Address of the Server:", "Welcome to the Chatter", JOptionPane.QUESTION_MESSAGE);
+    public Client(){
+        try {
+            String[] serverInformation = getServerAddress();
+            String strIP = serverInformation[0];
+            int intPort = Integer.parseInt(serverInformation[1]);
+            run(strIP, intPort);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    private void run() throws IOException
+    public Client(int intPort){
+        try {
+            run("127.0.0.1", intPort);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static String[] getServerAddress()
+    {
+        JTextField ip = new JTextField();
+        JTextField port = new JTextField();
+        final JComponent[] inputs = new JComponent[]{
+          new JLabel("Server IP: "),
+          ip,
+          new JLabel("Server Port: "),
+          port
+        };
+        int result = JOptionPane.showConfirmDialog(null, inputs, "Server Connection Information", JOptionPane.PLAIN_MESSAGE);
+        if (result == JOptionPane.OK_OPTION) 
+        {
+            String strHost = ip.getText();
+            String strPort = port.getText();
+            return new String[]{strHost, strPort};
+        }
+        return null;
+    }
+    
+    private void run(String strIP, int intPort) throws IOException
     {
 
     // Make connection and initialize streams
-    String serverAddress = getServerAddress();
-    Socket socket = new Socket(serverAddress, 9001);
+    Socket socket = new Socket(strIP, intPort);
     out = new ObjectOutputStream(socket.getOutputStream());
     out.flush();
     in = new ObjectInputStream(socket.getInputStream());
